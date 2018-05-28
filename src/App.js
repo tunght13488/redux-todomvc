19a9c981a089ff16d5ad103d7c6f971bef13b295
Ha The Tung <tunght13488@gmail.com>
2018-05-28 16:46:21 +0800

    fix test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './App.css';
 import Footer from './components/Footer';
 import AddTodo from './containers/AddTodo';
 import VisibleTodoList from './containers/VisibleTodoList';
+import todoApp from './reducers';
 
-function App() {
-  return (
+const store = createStore(
+  todoApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const App = () => (
+  <Provider store={store}>
     <section className="todoapp">
       <header className="header">
         <h1>todos</h1>
@@ -14,7 +22,7 @@ function App() {
       <VisibleTodoList/>
       <Footer/>
     </section>
-  );
-}
+  </Provider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './App';
 import './index.css';
-import todoApp from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-  todoApp,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
-  , document.getElementById('root'),
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
